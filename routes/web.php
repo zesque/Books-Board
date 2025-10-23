273c1f77d22065f9daaa9565228cb0f462f758cf
zesque <elnur.garaev@bk.ru>
2025-10-23 14:51:19 +0300

    Коммит маршрутов файла web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainPageController::class, 'show_main']);
+Route::get('book/{book_number', [BookController::class, 'show_book']);
+Route::delete('/book/{book_number}', [BookController::class, 'delete_book']);
+Route::get('/author{author_name}', [AuthorController::class, 'show_author']);
+Route::get('/auth', [AutheficationController::class, 'show_authentification']);
+Route::get('/auth/{user_name}', [UserController::class, 'show_user']);
+Route::get('/authors', [AuthorsController::class, 'show_authors']);
+Route::post('/new-book', [NewBookController::class, 'create_new-book']);
+Route::put('/new-book', [NewBookController::class, 'crete_new-book']);
+
