829a392648b7ed91d58f6ffe28b7c3df3e4d02c8
zesque <elnur.garaev@bk.ru>
2025-10-24 13:11:19 +0300

    Исправленный файл с маршрутами

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MainPageController::class, 'show_main']);
 Route::get('/book/{book_number}', [BookController::class, 'show_book']);
 Route::delete('/book/{book_number}', [BookController::class, 'delete_book']);
-Route::get('/author{author_name}', [AuthorController::class, 'show_author']);
-Route::get('/auth', [AutheficationController::class, 'show_authentification']);
+Route::get('/author/{author_name}', [AuthorController::class, 'show_author']);
+Route::get('/auth', [AuthentificationController::class, 'show_authentification']);
 Route::get('/auth/{user_name}', [UserController::class, 'show_user']);
 Route::get('/authors', [AuthorsController::class, 'show_authors']);
-Route::post('/new-book', [NewBookController::class, 'create_new-book']);
-Route::put('/new-book', [NewBookController::class, 'update_new-book']);
+Route::post('/new-book', [NewBookController::class, 'create_new_book']);
+Route::put('/new-book', [NewBookController::class, 'update_new_book']);
 
