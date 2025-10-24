3007333cf62fdeb147b18e5a5a7201df34d255ad
zesque <elnur.garaev@bk.ru>
2025-10-24 16:19:41 +0300

    Исправление файла с маршрутами: теперь маршрут, ведущий на всех авторов и маршрут, ведущий к автору из страницы с определенной книгой, стали именованными и принадлежат одному контроллеру AuthorController

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainPageController::class, 'show_main']);
-Route::get('/book/{book_number}', [BookController::class, 'show_book']);
-Route::delete('/book/{book_number}', [BookController::class, 'delete_book']);
-Route::get('/author/{author_name}', [AuthorController::class, 'show_author']);
+Route::get('/{book_number}', [BookController::class, 'show_book']);
+Route::delete('/{book_number}', [BookController::class, 'delete_book']);
+Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
+Route::get('/authors/{author}', [AuthorController::class, 'show'])->name('authors.show');
 Route::get('/auth', [AuthentificationController::class, 'show_authentification']);
 Route::get('/auth/{user_name}', [UserController::class, 'show_user']);
-Route::get('/authors', [AuthorsController::class, 'show_authors']);
 Route::post('/new-book', [NewBookController::class, 'create_new_book']);
 Route::put('/new-book', [NewBookController::class, 'update_new_book']);
 
